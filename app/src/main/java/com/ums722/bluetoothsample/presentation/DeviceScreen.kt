41000b1b674a462e89d9055789980927bd5987e7
이상훈 <sanghoon.lee@ikoob.com>
2023-04-28 11:09:06 +0900

    2023 4월 28일
    추가
    권한 관련 로직 추가함
    
    todo

--- app/src/main/java/com/ums722/bluetoothsample/presentation/DeviceScreen.kt
+++ app/src/main/java/com/ums722/bluetoothsample/presentation/DeviceScreen.kt
@@ -1,7 +1,85 @@
 package com.ums722.bluetoothsample.presentation
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ums722.bluetoothsample.domain.model.BluetoothDevice
 
 @Composable
-fun DeviceScreen() {
+fun DeviceScreen(
+    state: BluetoothUiState,
+    onStatScan: () -> Unit,
+    onStopScan: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = onStatScan) {
+                Text(text = "Start Scan")
+            }
+
+            Button(onClick = onStopScan) {
+                Text(text = "Stop Scan")
+            }
+        }
+    }
+}
+
+@Composable
+fun BluetoothDeviceList(
+    pairedDevices: List<BluetoothDevice>,
+    scannedDevices: List<BluetoothDevice>,
+    onClick: (BluetoothDevice) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    LazyColumn(modifier = modifier) {
+
+        item {
+            Text(
+                text = "paired Devices",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        items(pairedDevices) { device ->
+            Text(text = device.name ?: "no name",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onClick(device) }
+                    .padding(16.dp)
+            )
+        }
+
+        item {
+            Text(
+                text = "scanned Devices",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        items(scannedDevices) { device ->
+            Text(text = device.name ?: "no name",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onClick(device) }
+                    .padding(16.dp)
+            )
+        }
+    }
 }
--- app/src/main/java/com/ums722/bluetoothsample/presentation/MainActivity.kt
+++ app/src/main/java/com/ums722/bluetoothsample/presentation/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.ums722.bluetoothsample.presentation
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,14 +23,50 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val bluetoothManager by lazy {
+        applicationContext.getSystemService(BluetoothManager::class.java)
+    }
+
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BluetoothSampleTheme {
+
                 val viewModel = hiltViewModel<BluetoothViewModel>()
                 val state by viewModel.state.collectAsState()
+
+                val enableBluetoothLauncher = registerForActivityResult(
+                    ActivityResultContracts.StartActivityForResult()
+                ) { /*  */ }
+
+                val permissionLauncher = registerForActivityResult(
+                    ActivityResultContracts.RequestMultiplePermissions()
+                ) { perms ->
+                    val canEnableBluetooth = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        perms[Manifest.permission.BLUETOOTH_CONNECT] == true
+                    } else true
+
+                    if (canEnableBluetooth && !isBluetoothEnabled){
+                        enableBluetoothLauncher.launch(
+                            Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                        )
+                    }
+                }
+
                 Surface(color = MaterialTheme.colors.background) {
-                    DeviceScreen()
+                    DeviceScreen(
+                        state = state,
+                        onStatScan = viewModel::startScan,
+                        onStopScan = viewModel::stopScan,
+                    )
                 }
             }
         }
